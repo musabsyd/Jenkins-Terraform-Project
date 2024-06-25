fc47c19feeb79b6fbd237ec7530755991b9be3c8
musabsyd <musabsyd@gmail.com>
2024-06-26 00:36:03 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -4,5 +4,5 @@ access_key = "AKIASG72NZK6L36NPAUN"
 secret_key = "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"
 }
 resource "aws_s3_bucket" "mybucket" {
-bucket = "msbbuckets5"
+bucket = "msbbucketssd5"
 }
