bef21173acb3b95bc21e127cd23d3097b4fa18fd
musabsyd <musabsyd@gmail.com>
2024-07-06 17:45:20 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,7 +1,5 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "${var.AWS_ACCESS_KEY_ID}"
-secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 resource "aws_s3_bucket" "mybucket" {
 bucket = "msbbucket5"
